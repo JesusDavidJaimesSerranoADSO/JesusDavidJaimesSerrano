9ecf3b33ad1bc74b7f81d7ada364bfbfd8937c0f
JesusDavidJaimesSerranoADSO <jdavidjaimess@gmail.com>
2022-11-16 08:18:18 -0500

    Taller 1 Ejercicio 5

--- taller 1/main.js
+++ taller 1/main.js
@@ -1,6 +1,7 @@
 addEventListener("DOMContentLoaded",e=>{
     let opcion = document.querySelector('.medio');
     let dist = document.querySelector('[name="dis"]')
+    let calcular = 0
     let rest = 0
     
     opcion.addEventListener("change", (e)=>{
@@ -12,28 +13,28 @@ addEventListener("DOMContentLoaded",e=>{
             rest = 60
         }
     })
-
-
+    
+    
     let unidad = document.querySelector('.unidad')
     unidad.addEventListener("change", (e)=>{
         let seleccion = e.target.value
         if( seleccion == "km"){
-            rest = 40
+            calcular = (dist.value / rest) * 60
         }
         else{
-            rest = 60
+            calcular = ((dist.value / 100) / rest) * 60
         }
-
+        
+        alert(calcular)
     })
 
-    let op = dist * rest
     let myform = document.querySelector("#myform");
 
     myform.addEventListener("submit", (e)=>{
         e.preventDefault()
         let result = document.querySelector(".rest")
         result.insertAdjacentHTML("beforeend",`
-        <p>${rest}</p>
+        <p>${calcular}</p>
         `)
     })
     
